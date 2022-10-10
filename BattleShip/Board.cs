6bd725741f9260296af99f8d5eedd04abe7d1168
alex <alexbarbu240@gmail.com>
2022-10-10 11:46:10 +0300

    [feat] Created the main classes and imported the Colorful Module for more color

---
+++ BattleShip/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public class Board
+    {
+        public int[,] myBoard { get; set; }
+        public int nRows;
+        public int nCols;
+
+        public Board(int rows, int cols)
+        {
+            nRows = rows;
+            nCols = cols;
+            myBoard = new int[rows, cols];
+        }
+    }
+}
---
+++ BattleShip/Display.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+
+namespace BattleShip
+{
+    public class Display
+    {
+        private Board board;
+        public Display(Board board)
+        {
+            this.board = board;
+        }
+
+       
+
+        public void PrintBoard()
+        {
+            Console.WriteLine();
+            Console.Write("    ");
+            for (int i = 0; i < board.nRows; i++)
+            {
+                Console.Write($"{(char)('A'+i)}  ", Color.Wheat);
+            }
+
+            for (int row = 0; row < board.nRows; row++)
+            {
+                Console.WriteLine();
+                Console.Write(row < 9 ? $" {row + 1} " : row + 1 + " ", Color.Wheat); ;
+
+                for (int col = 0; col < board.nCols; col++)
+                {
+                    if (board.myBoard[row,col] == 0)
+                    {
+                        
+                        Console.Write(" ~ ", Color.CadetBlue);
+                    }
+                }
+
+            }
+        }
+    }
+}
